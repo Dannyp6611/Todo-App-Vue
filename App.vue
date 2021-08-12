68f600801f6cc40a3ff32c9deb22ce2427f9e54a
Dannyp6611 <54036518+Dannyp6611@users.noreply.github.com>
2021-08-12 19:26:41 +0100

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  max-width: 600px;
+  width: 80%;
+  margin: 40px auto;
+}
+
+</style>
